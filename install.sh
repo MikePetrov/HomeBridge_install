c674c2f702368d673a309eedad927709592dd91e
MikePetrov <msp@netgroup.su>
2018-09-25 09:09:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+# отключение swap файла
+sudo dphys-swapfile swapoff
+sudo dphys-swapfile uninstall
+sudo update-rc.d dphys-swapfile remove
+
+# удаление не нужных пакетов
+sudo apt autoremove --purge
+
+# добавление репозитория NodeJs
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+# установка не достающих пакетов
 sudo apt install -y nodejs libavahi-compat-libdnssd-dev
+
+# копирование файлов конфигурации 
 mkdir ~/.homebridge && cp config.json ~/.homebridge
-
-sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
-
 sudo cp homebridge.service /etc/systemd/system/
+
+# установка модулей home bridge
+## web
+sudo npm i -g --unsafe-perm homebridge homebridge-config-ui-x
+## ip камеры
+## 
+
+# запуск в качестве демона
 sudo systemctl daemon-reload
 sudo systemctl enable homebridge
 sudo systemctl start homebridge
