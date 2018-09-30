de2df5bfb8e4b46c71bcb749cfac6d1df763e721
MikePetrov <msp@netgroup.su>
2018-09-30 22:31:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
- #!/bin/bash
+#!/bin/bash
 set -o nounset
 set -o errexit
+
+Color_Off='\033[0m'
+Color_Red='\033[0;31m'
+Color_Green='\033[0;32m'
+Color_Blue='\033[0;34m'
+Color_Cyan='\033[0;36m'
 
 # смена hostname на HomeBridge
 oldname=$(cat /etc/hostname)
 sudo sed -i "s/$oldname/HomeBridge/g" /etc/hosts
 sudo sed -i "s/$oldname/HomeBridge/g" /etc/hostname
+
 
 # отключение swap файла
 sudo dphys-swapfile swapoff
@@ -18,19 +25,21 @@ sudo apt autoremove --purge
 # установка не достающих пакетов
 sudo apt install -y libavahi-compat-libdnssd-dev mc xz-utils
 
+ver=$(node -v)
+echo -e "Current Node version is ${Color_Cyan}$(Ver)${Color_Off}"
 # устанавливаем NodeJs-current (актуальная сейчас)
 cd ~
 wget https://nodejs.org/dist/v10.11.0/node-v10.11.0-linux-armv6l.tar.xz
 tar -xvJf ~/node-v10.11.0-linux-armv6l.tar.xz
 ver=$(~/node-v10.11.0-linux-armv6l/bin/node -v)
-echo "Unpacking version: $ver"
+echo -e "${Color_Green}Unpacking version: $ver ${Color_Off}"
 cd ~/node-v10.11.0-linux-armv6l
 sudo cp -R * /usr/local/
 export PATH=$PATH:/usr/local/bin
 ver=$(node -v)
-echo "System version NodeJS: $ver"
+echo -e "${Color_Green}System version NodeJS: $ver ${Color_Off}"
 ver=$(npm -v)
-echo "System version NPM: $ver"
+echo -e "${Color_Green}System version NPM: $ver ${Color_Off}"
 
 # копирование файлов конфигурации 
 mkdir -p ~/.homebridge
@@ -53,3 +62,6 @@ sudo systemctl start homebridge
 
 # перезагрузка
 sudo shutdown -r
+
+# confirm version
+echo -e "${Color_Green}Successfully installed. HomeBridge version is: $Ver${Color_Off}"
